272e56dfae0b48e8c17adb039d5e1fc8321fd2d1
tumjo <tumikku@gmail.com>
2020-11-05 19:03:38 +0200

    Add 1.5

--- part1/kurssitiedot/src/index.js
+++ part1/kurssitiedot/src/index.js
@@ -6,7 +6,6 @@ const Header = (props) => {
 };
 
 const Content = (props) => {
-  console.log(props);
   return (
     <div>
       <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
@@ -25,7 +24,6 @@ const Part = (props) => {
 };
 
 const Total = (props) => {
-  console.log(props);
   return (
     <p>
       Number of exercises{" "}
@@ -37,27 +35,29 @@ const Total = (props) => {
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
