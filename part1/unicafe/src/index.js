fe1af9f2e1e03cc8798daf25c28a0d1530a8f215
tumjo <tumikku@gmail.com>
2020-11-11 15:41:23 +0200

    Add 1.6-1.10

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+
+
+const Statistics = (props) => {
+  const {good, neutral, bad} = props;
+  if((good+neutral+bad)!==0){
+    return (
+      <>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={good+neutral+bad} />
+        <StatisticLine text="average" value={(-bad+good)/(good+neutral+bad)} />
+        <StatisticLine text="positive" value={good/(good+neutral+bad)*100 + " %"} />
+      </>
+      )
+    }
+    return (
+      <p>no feedback given</p>
+    )
+  }
+
+
+const StatisticLine = (props) => {
+  const {text, value} = props;
+
+  return (
+    <p>{text} {value}</p>
+  )
+}
+const Button = (props) => {
+  const {handleClick, text} = props;
+  return (
+    <button onClick={handleClick}>{text}</button>
+  )
+
+ }
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={()=>{setGood(good+1)}} text="good" />
+      <Button handleClick={()=>{setNeutral(neutral+1)}} text="neutral" />
+      <Button handleClick={()=>{setBad(bad+1)}} text="bad" />
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
+
